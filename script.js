297c04a5490724b78ecadfc703f1afa276e74c93
Arpit Kumar Singh <singharpit302@gmail.com>
2023-05-24 01:25:21 +0530

    Basic layout

---
+++ script.js
@@ -0,0 +1,11 @@
+const Web3 = require('web3');
+const MyContract = require('./build/contracts/MyContract.json');
+const address = '0x7Af0Da3bF23698025AAf3Bd3498a18b1Fcfb9175'; 
+const privateKey = '0f1543a73ae2d980fe6a4feb045bb9b99398673e3b7e8c5e6991ac6fe4a8becf';
+const infuraUrl = 'https://ropsten.infura.io/v3/...';
+
+const init1 = async () => {
+
+}
+
+init1();
