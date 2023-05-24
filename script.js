14c19066f339922f79f46cc8e3a17fa77d99aa28
Arpit Kumar Singh <singharpit302@gmail.com>
2023-05-24 12:57:14 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const Web3 = require('web3');
 const MyContract = require('./build/contracts/MyContract.json');
-const address = '0x7Af0Da3bF23698025AAf3Bd3498a18b1Fcfb9175'; 
-const privateKey = '0f1543a73ae2d980fe6a4feb045bb9b99398673e3b7e8c5e6991ac6fe4a8becf';
-const infuraUrl = 'https://ropsten.infura.io/v3/...';
+const address = '0xB91a216dCa621Cf4CBb314551ABc3019481C6493'; 
+const privateKey = '47ba641b40c35003958adf3b05a64988d4ba29ac99f148e110ff9e04304be812';
+const infuraUrl = 'https://rinkeby.infura.io/v3/74aa9a15e2524f6980edb8a377301f3c';
 
 const init1 = async () => {
+    const web3 = new Web3(infuraUrl);
+    const networkId = await web3.eth.net.getId();
+    const myContract = new web3.eth.Contract(
+        MyContract.abi,
+        MyContract.networks[networkId].address
+    );
 
+    const tx = myContract.methods.set(1);
 }
 
 init1();
